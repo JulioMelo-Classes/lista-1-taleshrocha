f1522f565e99c73bcdeb01748f12574e0ca2205f
taleshrocha <taleshrochaz@gmail.com>
2021-06-17 22:16:44 -0300

    Ponto_em_retangulo_2 done

--- ponto_em_retangulo_2/src/function.cpp
+++ ponto_em_retangulo_2/src/function.cpp
@@ -1,11 +1,17 @@
 #include "function.h"
 
-/*! 
- * Verifica se um ponto está dentro de um retângulo.
- */
-location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
-{
-    // TODO: Adicione seu código aqui.
+location_t pt_in_rect(const Ponto &IE, const Ponto &SD, const Ponto &P){
+    bool inSD, inIE;
+    inSD = inIE = false;
+    if(SD.x > P.x and SD.y > P.y) inSD = true; //Inside of the right-superior side
+    if(IE.x < P.x and IE.y < P.y) inIE = true; //Inside of the left-inferior side
 
-    return location_t::OUTSIDE;
+    if(inSD and inIE) return location_t::INSIDE;
+    else{
+        inSD = inIE = false;
+        if(SD.x >= P.x and SD.y >= P.y) inSD = true; //Inside or in the edge of the right-superior side
+        if(IE.x <= P.x and IE.y <= P.y) inIE = true; //Inside or in the edge of the left-inferior side
+        if(inSD and inIE) return location_t::BORDER;
+        else return location_t::OUTSIDE;
+    }
 }
--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -1,27 +1,59 @@
-/*!
- * @brief Implementação do Ponto em Retângulo V2.
- * @author selan
- * @data June, 6th 2021
- */
-
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
 #include <algorithm>
-using std::min;
-using std::max;
-
 #include "function.h"
+using namespace std;
 
-//=== Funções
+void fix(Ponto &IE, Ponto &SD){
+    bool xGood = false;
+    bool yGood = false;
+    Ponto aux;
+    if(SD.x > IE.x) xGood = true;
+    if(SD.y > IE.y) yGood = true;
+    if(!xGood){
+        aux.x = IE.x;
+        IE.x = SD.x;
+        SD.x = aux.x;
+    }
+    if(!yGood){
+        aux.y = IE.y;
+        IE.y = SD.y;
+        SD.y = aux.y;
+    }
+}
 
-// Coloque aqui qualquer função auxiliar que desejar.
-
+void prtres(int res){
+    if(res == 0) cout<<"inside"<<endl;
+    else if(res == 1) cout<<"border"<<endl;
+    if(res == 2) cout<<"outside"<<endl;
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int x, y;
+    int res = 99;
+    int i = 0;
+    //int x[3];
+    //int y[3];
+    Ponto P[3];
+    while(cin >> ws >> x >> y){ //Reads a undetermined number of lines
+        if(!(-1000 <= x and x <= 1000)) return 0;
+        else if(!(-1000 <= y and y <= 1000)) return 0;
+        //cout<<x<<y<<endl;
+        P[i] = {x, y};
+        if(i < 2) i++;
+        else{
+            i = 0;
+            fix(P[0], P[1]);
+            res = pt_in_rect(P[0], P[1], P[2]);
+            //cout<<"RESULTADO: "<<res<<endl;
+            //cout<<res<<endl;
+            prtres(res);
+        }
+    }
+
+    /*for(int i = 0; i < 3; ++i){
+       cout<<i<<" "<<P[i].x<<P[i].y<<endl; 
+    }*/
 
     return 0;
 }
